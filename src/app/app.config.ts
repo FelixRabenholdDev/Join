8b29566f7c3eac8876d153e217acf219c65d98ba
Felix R <nikojin@gmx.net>
2025-12-05 11:47:34 +0100

    feat: add firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "join-db-ee5a8", appId: "1:1071337539188:web:a82b5c20d18417d9423321", storageBucket: "join-db-ee5a8.firebasestorage.app", apiKey: "AIzaSyBDbp41sPTcemNlIZoP9lyE037AktuztqY", authDomain: "join-db-ee5a8.firebaseapp.com", messagingSenderId: "1071337539188", projectNumber: "1071337539188", version: "2" })), provideFirestore(() => getFirestore())
   ]
 };
