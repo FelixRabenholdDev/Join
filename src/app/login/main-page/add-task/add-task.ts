1cfbd52ec01ec22cd5bd8c4c7968f7a512e2b40b
DanielPetrowski <petrowskidaniel@web.de>
2025-12-18 15:47:22 +0100

    add task added

--- src/app/login/main-page/add-task/add-task.ts
+++ src/app/login/main-page/add-task/add-task.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-add-task',
+  standalone:true,
   imports: [],
   templateUrl: './add-task.html',
   styleUrl: './add-task.scss',
 })
 export class AddTask {
+  priority: 'urgent' | 'medium' | 'low' | null = null;
 
+  setPriority(p: 'urgent' | 'medium' | 'low') {
+    if (this.priority === p) {
+      this.priority = null;
+    } else {
+      this.priority = p;
+    }
+  }
 }
+
+
