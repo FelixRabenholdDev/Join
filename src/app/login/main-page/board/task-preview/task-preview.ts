ace44146590edfbc54b508f77d811b000d86f06b
Jürgen Malinowski <jm-malle@gmx.de>
2025-12-18 15:37:45 +0100

    FEAT: Drag & drop integrated for testing purposes, without any further functionalities except dragging and repositioning a TASK.

--- src/app/login/main-page/board/task-preview/task-preview.ts
+++ src/app/login/main-page/board/task-preview/task-preview.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  CdkDrag,
+  CdkDropList,
+  CdkDropListGroup,
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-task-preview',
-  imports: [],
+  imports: [
+    CommonModule,
+    CdkDrag,
+    CdkDropList,
+    CdkDropListGroup
+  ],
   templateUrl: './task-preview.html',
   styleUrl: './task-preview.scss',
 })
 export class TaskPreview {
 
+  todoTasks: string[] = ['Test Task'];
+  inProgressTasks: string[] = [];
+  awaitFeedbackTasks: string[] = [];
+  doneTasks: string[] = [];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 }
