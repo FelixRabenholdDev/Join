82931422b15301e9873b94a42639e803ed3c6ff3
Rohric <emilmarsal@icloud.com>
2025-12-19 18:11:46 +0100

    DirtyFix to show TaskDialog!!!

--- src/app/login/main-page/board/board.ts
+++ src/app/login/main-page/board/board.ts
@@ -1,10 +1,11 @@
 import { Component, ViewChild } from '@angular/core';
 import { TaskList } from './task-list/task-list';
 import { DialogAddTask } from './dialog-add-task/dialog-add-task';
+import { DialogShowEditTask } from './task-preview/dialog-show-edit-task/dialog-show-edit-task';
 
 @Component({
   selector: 'app-board',
-  imports: [TaskList, DialogAddTask],
+  imports: [TaskList, DialogAddTask,DialogShowEditTask],
   templateUrl: './board.html',
   styleUrl: './board.scss',
 })
