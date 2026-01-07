f98320daeeab66e56a05ea309d45d9cb5b55b6e8
DanielPetrowski <petrowskidaniel@web.de>
2026-01-07 23:07:20 +0100

    add  HTML;SCSS,TS configuration Routes

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { MainPage } from './main-page/main-page';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-login',
-  imports: [MainPage,],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
 
+  constructor(private router: Router) {}
+
+  guestLogin() {
+  // Nach Klick auf Guest Login → Summary anzeigen
+  this.router.navigate(['/summary']);
+
+}
 }
